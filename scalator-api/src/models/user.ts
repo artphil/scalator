e0f14d8570f46a5cc55d4210c8ad32305e89aff9
artphil <artphil.dev@gmail.com>
2024-12-23 18:33:23 -0300

    change id type

--- scalator-api/src/models/user.ts
+++ scalator-api/src/models/user.ts
@@ -1,8 +1,9 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database";
+import { v4 as uuidv4 } from "uuid";
 
 class User extends Model {
-  public id!: number;
+  public id!: string;
   public name!: string;
   public email!: string;
 }
@@ -10,8 +11,8 @@ class User extends Model {
 User.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+      type: DataTypes.UUID,
+      defaultValue: uuidv4,
       primaryKey: true,
     },
     name: {
